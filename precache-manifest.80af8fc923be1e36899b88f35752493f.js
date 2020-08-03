44ca070d2f16939a7a0562a782b13925bf950bd2
aidera <hope.aidera@gmail.com>
2020-08-03 16:46:23 +0300

    Updates

---
+++ precache-manifest.80af8fc923be1e36899b88f35752493f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c786648bbfea18d5328904c1c0dfcc5",
+    "url": "/japanese-landscape/index.html"
+  },
+  {
+    "revision": "e214ae5a0bf93d41d36e",
+    "url": "/japanese-landscape/static/css/main.6673dee2.chunk.css"
+  },
+  {
+    "revision": "1eb005eb85a181c873e0",
+    "url": "/japanese-landscape/static/js/2.b7e35764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/japanese-landscape/static/js/2.b7e35764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e214ae5a0bf93d41d36e",
+    "url": "/japanese-landscape/static/js/main.db761774.chunk.js"
+  },
+  {
+    "revision": "718629422336dacd5b87",
+    "url": "/japanese-landscape/static/js/runtime-main.7bc63573.js"
+  },
+  {
+    "revision": "ec0ba0ac0e816c6de0ba1167e634e8ce",
+    "url": "/japanese-landscape/static/media/art-overlay.ec0ba0ac.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f07703dd30ecd7683acffa1c1c405be1.js"
+  "/japanese-landscape/precache-manifest.80af8fc923be1e36899b88f35752493f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/japanese-landscape/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
