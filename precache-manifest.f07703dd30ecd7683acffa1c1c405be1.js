b5bd3eeda3f4e7086085923a41ce8f18df4e138f
aidera <hope.aidera@gmail.com>
2020-08-03 16:41:01 +0300

    Updates

---
+++ precache-manifest.f07703dd30ecd7683acffa1c1c405be1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe3f57a996c7d90955be02c623c6d4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bf219d25d1453f8d8eb",
+    "url": "/static/css/main.fd8dce6a.chunk.css"
+  },
+  {
+    "revision": "1eb005eb85a181c873e0",
+    "url": "/static/js/2.b7e35764.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7e35764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf219d25d1453f8d8eb",
+    "url": "/static/js/main.7e0eb7e3.chunk.js"
+  },
+  {
+    "revision": "9b4b7f258a07332f8eff",
+    "url": "/static/js/runtime-main.ef4a55f9.js"
+  },
+  {
+    "revision": "ec0ba0ac0e816c6de0ba1167e634e8ce",
+    "url": "/static/media/art-overlay.ec0ba0ac.png"
+  }
+]);
